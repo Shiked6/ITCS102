d47f9c117244398302d8e0e16302f4f653541cfb
Joshua Ezekiel A. Agawin <86836914+Shiked6@users.noreply.github.com>
2024-12-16 21:46:19 +0800

    Update menu_agawin_draft.py

--- menu_agawin_draft.py
+++ menu_agawin_draft.py
@@ -27,15 +27,15 @@ Code Compilation 2024 for ITCS102
 code_challange1.py = 1               activity1.py = 101         activity14.py = 114
 code_challange2.py = 2               activity2.py = 102         activity15.py = 115
 code_challange3.py = 3               activity3.py = 103         activity16.py = 116
-code_challange4.py = 4               activity4.py = 104         activity17.py = 
-code_challange5.py = 5               activity5.py = 105                         
-code_challange6.py = 6               activity6.py = 106
-code_challange7.py = 7               activity7.py = 107
-code_challange8.py = 8               activity8.py = 108                                             
-code_challange9.py = 9               activity9.py = 109
-code_challange10.py = 10             activity10.py = 110
-code_challange11.py = 11             activity11.py = 111
-code_challange12.py = 12             activity12.py = 112
+code_challange4.py = 4               activity4.py = 104         activity17.py = 117
+code_challange5.py = 5               activity5.py = 105         activity18.py = 118                         
+code_challange6.py = 6               activity6.py = 106         activity19.py = 119
+code_challange7.py = 7               activity7.py = 107         activity20.py = 120
+code_challange8.py = 8               activity8.py = 108         activity21.py = 121                                             
+code_challange9.py = 9               activity9.py = 109         activity22.py = 122
+code_challange10.py = 10             activity10.py = 110        activity23.py = 123
+code_challange11.py = 11             activity11.py = 111        activity24.py = 124
+code_challange12.py = 12             activity12.py = 112        activity25.py = 125
 code_challange13.py = 13             activity13.py = 113
 code_challange14.py = 14
 code_challange15.py = 15
